2d22a6be3a3cd61d9c1efaa93d10a9766ad75019
Kartik <aggarwalkartik96@gmail.com>
2020-07-22 15:28:11 +0530

    Updates

---
+++ precache-manifest.a81812bdb3c555197f75d567b74c358d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dfe6fb0f4643957eea3c74820cd8854",
+    "url": "/useractivity/index.html"
+  },
+  {
+    "revision": "bd60ac12d91c2bd111c5",
+    "url": "/useractivity/static/css/2.19978504.chunk.css"
+  },
+  {
+    "revision": "2b0fc5917781dc2c4ac6",
+    "url": "/useractivity/static/css/main.ade39df7.chunk.css"
+  },
+  {
+    "revision": "bd60ac12d91c2bd111c5",
+    "url": "/useractivity/static/js/2.87b47ffa.chunk.js"
+  },
+  {
+    "revision": "78e572a38ce62fad1af1cd75e03f15bb",
+    "url": "/useractivity/static/js/2.87b47ffa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b0fc5917781dc2c4ac6",
+    "url": "/useractivity/static/js/main.600f38e7.chunk.js"
+  },
+  {
+    "revision": "9d0a951773bc9237fb4c",
+    "url": "/useractivity/static/js/runtime-main.52df06d6.js"
+  },
+  {
+    "revision": "eae6ce304e1fe2dc4c012e679a701dc1",
+    "url": "/useractivity/static/media/Usericon.eae6ce30.png"
+  }
+]);
